4eda87988ebd70878fc2d1dfa8cc4d8a43dbda46
Adnan khalil <adnankhalil131@gmail.com>
2026-05-21 22:57:59 +0300

    Create Eloquent models and relationships

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'restaurant_id',
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'image',
+        'is_available',
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
